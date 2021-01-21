269cd04b1fd7f152c0bf75a532b17a6b6456670f
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-21 23:47:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
-class Student 
+class Student < User
+    # attr_accessor :first_name, :last_name 
+    
+    attr_accessor :knowledge
+
+    def initialize
+    @knowledge = []
+    end
+
+    def learn(knowledge)
+    @knowledge << knowledge
+    end
+
+    def knowledge
+    @knowledge
+    end
+    
+    # def initialize(first_name, last_name)
+    # @first_name = first_name
+    # @last_name = last_name
+    # @@all << self
+    # end
+  
 
 end
